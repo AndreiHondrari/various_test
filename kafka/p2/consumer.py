60d1e9082008777b46b4c77887541ec0c2288e9f
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2021-01-12 18:36:22 +0100

    Updates.

--- kafka/p2/consumer.py
+++ kafka/p2/consumer.py
@@ -1,6 +1,8 @@
 import random
+import time
 
 from kafka import KafkaConsumer
+from tenacity import Retrying, stop_after_attempt, wait_fixed, RetryError
 
 consumer = KafkaConsumer(bootstrap_servers="localhost:9092")
 
@@ -9,25 +11,36 @@ print("--- Kafka Consumer (replay from 0) ---\n")
 
 # acquire topics
 topics = list(consumer.topics())
+consumer.poll(0)
 
-topics_print = '\n'.join(topics)
-print(f"Subscribing to following topics: {topics_print}")
+topics_print = '\n'.join([f"\t{t}" for t in topics])
+print(f"Subscribing to following topics:\n{topics_print}")
 
 # subscribe to topics
-for topic in topics:
-    consumer.subscribe(topic)
+consumer.subscribe(topics)
+consumer.topics()  # force assignment
 
 # !!! Get assignments and define partitions
-print("Get assignments ...")
-for topic in topics:
-    consumer.partitions_for_topic(topic)
+print("\nGet assignments ...")
+assignments = None
 
-assignments = consumer.assignment()
-assignments_print = '\n'.join([f"{tp.topic}: {tp.partition}" for tp in assignments])
-print(f"Assignments: {assignments_print}")
+try:
+    for attempt in Retrying(wait=wait_fixed(3), stop=stop_after_attempt(3)):
+        with attempt:
+            assignments = consumer.assignment()
+            print(f"Current assignments: {assignments}")
+            if len(assignments) == 0:
+                raise Exception
+
+except RetryError:
+    print("Could not obtain topic assignment. Exiting ...")
+    exit(1)
+
+assignments_print = '\n'.join([f"\t{tp.topic}: {tp.partition}" for tp in assignments])
+print(f"\nAssignments:\n{assignments_print}")
 
 # rollback
-print("Rollback to 0 all topics")
+print("\nRollback to 0 all topics")
 for topic_partition in assignments:
     consumer.seek(topic_partition, 0)  # put from beginning
 

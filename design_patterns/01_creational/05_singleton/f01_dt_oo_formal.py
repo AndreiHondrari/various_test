763193dd65b99abc6bba92a8314db6efe1c8a7ea
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-03-10 13:28:40 +0100

    DP Better singleton.

--- design_patterns/01_creational/05_singleton/f01_dt_oo_formal.py
+++ design_patterns/01_creational/05_singleton/f01_dt_oo_formal.py
@@ -3,25 +3,56 @@
 from typing import Optional
 
 
+class SingletonAlreadyInstantiatedError(Exception):
+    pass
+
+
 class SomethingUnique:
+    """
+    The singleton class.
+    Overrides __new__ by creating only one instance per class.
+    Raises an exception in the case of additional instantiation.
+    """
 
     __instance: Optional['SomethingUnique'] = None
 
-    def __new__(cls, name: str) -> 'SomethingUnique':
-        if cls.__instance is None:
-            cls.__instance = super(SomethingUnique, cls).__new__(cls)
-
+    @classmethod
+    def get_instance(cls) -> Optional['SomethingUnique']:
         return cls.__instance
+
+    def __new__(cls, name: str) -> 'SomethingUnique':
+        if cls.__instance is not None:
+            raise SingletonAlreadyInstantiatedError
+
+        instance: 'SomethingUnique' = super(SomethingUnique, cls).__new__(cls)
+        cls.__instance = instance
+        return instance
 
     def __init__(self, name: str):
         self.name = name
 
 
 if __name__ == "__main__":
+    # first instantiation
     o1 = SomethingUnique('AAA')
-    o2 = SomethingUnique('BBB')
-    o3 = SomethingUnique('CCC')
-
     print(f"o1: {id(o1)}: {o1.name}")
-    print(f"o2: {id(o2)}: {o2.name}")
-    print(f"o3: {id(o3)}: {o3.name}")
+
+    # subsequent instantiations
+    try:
+        o2 = SomethingUnique('BBB')
+    except SingletonAlreadyInstantiatedError:
+        print("o2 exception detected")
+        pass
+
+    try:
+        o3 = SomethingUnique('CCC')
+    except SingletonAlreadyInstantiatedError:
+        print("o3 exception detected")
+        pass
+
+    # get the already existing instance
+    o4: Optional[SomethingUnique] = SomethingUnique.get_instance()
+    if o4 is not None:
+        print(f"o4: {id(o4)}: {o4.name}")
+    else:
+        raise Exception("o4 shouldn't be none!")

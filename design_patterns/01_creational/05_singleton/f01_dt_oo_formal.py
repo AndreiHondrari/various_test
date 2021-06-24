81c17ebb4afe8a4f4fbdeecdd78ead17bad36a9c
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-06-24 20:49:18 +0200

    Singleton improvements.

--- design_patterns/01_creational/05_singleton/f01_dt_oo_formal.py
+++ design_patterns/01_creational/05_singleton/f01_dt_oo_formal.py
@@ -1,58 +1,33 @@
 
 
-from typing import Optional
+from typing import Optional, Type
 
 
-class SingletonAlreadyInstantiatedError(Exception):
-    pass
-
-
-class SomethingUnique:
+class Singleton:
     """
-    The singleton class.
-    Overrides __new__ by creating only one instance per class.
-    Raises an exception in the case of additional instantiation.
+    Our singleton class that creates only one instance.
+    Notice that if you had instantiation arguments (args, kwargs), then
+    every subsequent Singleton() call with different arguments would not be
+    deterministic or make sense, since only the first instance is relevant.
+    You would have to raise an exception for different instantiation
+    parameters, or create different instance for
+    each combination of parameters.
     """
 
-    __instance: Optional['SomethingUnique'] = None
+    _instance: Optional['Singleton'] = None
 
-    @classmethod
-    def get_instance(cls) -> Optional['SomethingUnique']:
-        return cls.__instance
+    def __new__(cls: Type['Singleton']) -> 'Singleton':
+        if cls._instance is None:
+            cls._instance = object.__new__(cls)
 
-    def __new__(cls, name: str) -> 'SomethingUnique':
-        if cls.__instance is not None:
-            raise SingletonAlreadyInstantiatedError
-
-        instance: 'SomethingUnique' = super(SomethingUnique, cls).__new__(cls)
-        cls.__instance = instance
-        return instance
-
-    def __init__(self, name: str):
-        self.name = name
+        return cls._instance
 
 
 if __name__ == "__main__":
-    # first instantiation
-    o1 = SomethingUnique('AAA')
-    print(f"o1: {id(o1)}: {o1.name}")
+    o1: Singleton = Singleton()
+    o2: Singleton = Singleton()
+    o3: Singleton = Singleton()
 
-    # subsequent instantiations
-    try:
-        o2 = SomethingUnique('BBB')
-    except SingletonAlreadyInstantiatedError:
-        print("o2 exception detected")
-        pass
-
-    try:
-        o3 = SomethingUnique('CCC')
-    except SingletonAlreadyInstantiatedError:
-        print("o3 exception detected")
-        pass
-
-    # get the already existing instance
-    o4: Optional[SomethingUnique] = SomethingUnique.get_instance()
-    if o4 is not None:
-        print(f"o4: {id(o4)}: {o4.name}")
-    else:
-        raise Exception("o4 shouldn't be none!")
+    print(f"o1 id: {id(o1)}")
+    print(f"o2 id: {id(o2)}")
+    print(f"o3 id: {id(o3)}")
--- design_patterns/01_creational/05_singleton/f10_dt_problem_singleton_as_internal_dependency.py
+++ design_patterns/01_creational/05_singleton/f10_dt_problem_singleton_as_internal_dependency.py
@@ -6,27 +6,28 @@ that only makes your class a whitebox, and not a proper module.
 """
 
 import abc
+from typing import Type, Optional, Any
 
 
 class AbstractFunctionality(abc.ABC):
 
     @abc.abstractmethod
-    def do_something(self):
+    def do_something(self) -> None:
         raise NotImplementedError
 
 
 class SomeSingleton(AbstractFunctionality):
 
-    _instance = None
+    _instance: Optional['SomeSingleton'] = None
 
     @classmethod
-    def get_instance(cls):
+    def get_instance(cls: Type['SomeSingleton']) -> 'SomeSingleton':
         if cls._instance is None:
             cls._instance = cls()
 
         return cls._instance
 
-    def do_something(self):
+    def do_something(self) -> None:
         print("I am a singleton !!")
 
 
@@ -35,7 +36,7 @@ class NotSingletonDependency(AbstractFunctionality):
     def __init__(self, name: str):
         self.name = name
 
-    def do_something(self):
+    def do_something(self) -> None:
         print(f"I am {self.name}")
 
 
@@ -44,10 +45,10 @@ class UglyCustom:
     A nasty class using singleton directly
     """
 
-    def __init__(self):
-        self._dependency = SomeSingleton.get_instance()
+    def __init__(self) -> None:
+        self._dependency: Any = SomeSingleton.get_instance()
 
-    def use_dependency(self):
+    def use_dependency(self) -> None:
         self._dependency.do_something()
 
 
@@ -56,10 +57,10 @@ class BeautifulCustom:
     A beautiful class using singleton as a passed dependency
     """
 
-    def __init__(self, dependency: SomeSingleton):
-        self._dependency = dependency
+    def __init__(self, dependency: Any):
+        self._dependency: Any = dependency
 
-    def use_dependency(self):
+    def use_dependency(self) -> None:
         self._dependency.do_something()
 
 
